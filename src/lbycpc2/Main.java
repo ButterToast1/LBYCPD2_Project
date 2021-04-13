291ca400e788e7870a1998def1d716a118530d03
Jian Lasam <jian_lasam@dlsu.edu.ph>
2021-04-13 16:35:03 +0800

    Made initial signUp.fxml design (not complete yet) Renamed Login.fxml to login.fxml Added new text fields and password field in signUp.fxml

--- src/lbycpc2/Main.java
+++ src/lbycpc2/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("CropServant");
         primaryStage.setScene(new Scene(root, 1440, 1024));
         primaryStage.show();
